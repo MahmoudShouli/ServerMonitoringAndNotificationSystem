6bab42e7b726e1ea240a9de1d8d8dc04a12456df
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-29 03:53:16 +0300

    Override ToString

--- ServerMonitoringAndNotificationSystem/ServerStatistics.cs
+++ ServerMonitoringAndNotificationSystem/ServerStatistics.cs
@@ -6,4 +6,12 @@ public class ServerStatistics
     public double AvailableMemory { get; set; } // in MB
     public double CpuUsage { get; set; }
     public DateTime Timestamp { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Timestamp:yyyy-MM-dd HH:mm:ss}] " +
+               $"CPU Usage: {CpuUsage:F2}%, " +
+               $"Used Memory: {MemoryUsage:F2} MB, " +
+               $"Available Memory: {AvailableMemory:F2} MB";
+    }
 }
