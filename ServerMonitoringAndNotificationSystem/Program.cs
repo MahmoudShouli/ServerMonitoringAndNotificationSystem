11bd712465bf3dbb1335382ded8e2ad5e98372ed
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-29 03:53:37 +0300

    Test the StatCollection service

--- ServerMonitoringAndNotificationSystem/Program.cs
+++ ServerMonitoringAndNotificationSystem/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace ServerMonitoringAndNotificationSystem;
+﻿using ServerMonitoringAndNotificationSystem.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace ServerMonitoringAndNotificationSystem;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var interval = int.Parse(config["ServerStatisticsConfig:SamplingIntervalSeconds"]);
+
+        var service = new StatCollectionService();
+
+        while (true)
+        {
+            Console.WriteLine(service.GetServerStatistics().ToString());
+            Thread.Sleep(interval*1000);
+        }
     }
 }
